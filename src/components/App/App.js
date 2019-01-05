7d68b70865994bbda74055a82133114f1ec27c42
Tyler <tpedora@me.com>
2019-01-05 14:45:33 -0700

    Added HTML/JSX/CSS to each of the components and starting commenting in key methods.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Playlist from '../Playlist/Playlist.js';
+import SearchResults from '../SearchResults/SearchResults.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+//Module to render all other components to index.js
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
       </div>
     );
   }
